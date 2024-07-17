9e31b65cf0e00ae99ffa2ece9e8584f78029380d
geek <elmainiaymane03@gmail.com>
2024-07-17 10:40:49 +0100

    push in the master

---
+++ src/test/java/dev/codex/redindiansnight/RedIndiansNightApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.codex.redindiansnight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedIndiansNightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
