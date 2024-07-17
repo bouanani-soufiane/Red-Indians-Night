4ef184f4b8144f66177785bdc1d70550127ffef7
geek <elmainiaymane03@gmail.com>
2024-07-17 10:25:28 +0100

    setup the project using gradle

---
+++ src/main/java/dev/codex/redindiansnight/RedIndiansNightApplication.java
@@ -0,0 +1,13 @@
+package dev.codex.redindiansnight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedIndiansNightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedIndiansNightApplication.class, args);
+    }
+
+}
