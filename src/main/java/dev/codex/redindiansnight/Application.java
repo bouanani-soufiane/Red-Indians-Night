5025854901a79cb9bf4f6f00b73bb070ac708277
geek <elmainiaymane03@gmail.com>
2024-07-17 10:33:09 +0100

    setup the application properties

--- src/main/java/dev/codex/redindiansnight/Application.java
+++ src/main/java/dev/codex/redindiansnight/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RedIndiansNightApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(RedIndiansNightApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
